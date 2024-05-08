b9a02b4ff053e1fe20fdd8a7639b2c12697f2a28
Abid Dar <abiddar1979@gmail.com>
2024-05-08 09:46:37 +0500

    files added

---
+++ app.js
@@ -0,0 +1,13 @@
+/* Task # 25 : Alien colours #1: Imagin an alien was just shot down in a game .Creat
+a variable called alien_colour and assign it with a value of `green` , `yellow` or `red`.
+
+*write an if statement to tesyt weather the alien's colour is green .If it is , print a massage that the player just earned 5 points .*/
+var alien_colour = "green";
+if (alien_colour === "green") {
+    console.log("congralutions! you just earned 5 points!");
+}
+/* Write one version of this program that passes the if test and another that fails.(The Version that fails will have no output.)*/
+alien_colour = "red";
+if (alien_colour === "green") {
+    console.log("congralujtions! you just earned 5 points");
+}
